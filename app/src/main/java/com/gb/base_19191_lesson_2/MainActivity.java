6ba7382fb20b1bb26dc6f684b4b856fbb4a1fcd0
Andej Nesterenko <mentatus.n@gmail.com>
2022-01-24 20:49:35 +0300

    урок 2 начали, разобрались с FrameLayout

---
+++ app/src/main/java/com/gb/base_19191_lesson_2/MainActivity.java
@@ -0,0 +1,17 @@
+package com.gb.base_19191_lesson_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ((ViewGroup) Button)
+    }
+}
