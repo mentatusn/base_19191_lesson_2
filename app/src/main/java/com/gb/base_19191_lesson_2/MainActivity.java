290f22a17447a6430d9c2d07a6324f580e15e308
Andej Nesterenko <mentatus.n@gmail.com>
2022-01-24 22:41:47 +0300

    урок 2, разобрались с ConstraintLayout

--- app/src/main/java/com/gb/base_19191_lesson_2/MainActivity.java
+++ app/src/main/java/com/gb/base_19191_lesson_2/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_table);
+        setContentView(R.layout.activity_constraint);
     }
 }
